e4c3ccc6b21961b4b83f9c36fa4c987fce19f5f1
tomekw6ywshshskw <110564381+tomekw6ywshshskw@users.noreply.github.com>
2024-08-08 11:52:12 +0200

    Create greetings.py

---
+++ Cogs/greetings.py
@@ -0,0 +1,21 @@
+
+from discord.ext import commands
+
+class Greetings(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        channel = discord.utils.get(member.guild.text_channels, name="powitania")
+        if channel:
+            await channel.send(f"Witaj na serwerze, {member.mention}!")
+
+    @commands.Cog.listener()
+    async def on_member_remove(self, member):
+        channel = discord.utils.get(member.guild.text_channels, name="pożegnania")
+        if channel:
+            await channel.send(f"{member.name} opuścił serwer.")
+
+def setup(bot):
+    bot.add_cog(Greetings(bot))

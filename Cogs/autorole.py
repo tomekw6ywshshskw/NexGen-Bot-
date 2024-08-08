182fd845f048dd48b263879129d15008062f80ca
tomekw6ywshshskw <110564381+tomekw6ywshshskw@users.noreply.github.com>
2024-08-08 11:51:13 +0200

    Update autorole.py

--- Cogs/autorole.py
+++ Cogs/autorole.py
@@ -1,1 +1,15 @@
+from discord.ext import commands
+import discord
 
+class AutoRole(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        role = discord.utils.get(member.guild.roles, name="Nowicjusz")
+        await member.add_roles(role)
+
+def setup(bot):
+    bot.add_cog(AutoRole(bot))
+  
